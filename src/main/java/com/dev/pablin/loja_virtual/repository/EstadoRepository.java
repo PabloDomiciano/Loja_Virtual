45e6f1ff6b59dddbab319dfdf4b0e0f3b3251a27
Pablo Henrique <pablo.domiciano2019@gmail.com>
2023-06-02 01:00:21 -0300

    Criacao da entidade estado

---
+++ src/main/java/com/dev/pablin/loja_virtual/repository/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.dev.pablin.loja_virtual.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.pablin.loja_virtual.entity.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long>{
+    
+}
