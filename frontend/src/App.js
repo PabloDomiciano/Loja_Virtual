1848a339b2307c8e8d9d415b86f354f1800f2701
Pablinn <pablo.domiciano2019@gmail.com>
2023-09-22 20:55:22 -0300

    Criando Componentes Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home/home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path ='/' Component={() => <Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+
   );
 }
 
---
+++ frontend/src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'primereact/button';
+
+const Home = () => {
+    return (
+        <>
+            <div className="card flex justify-content-center">
+                <Button label="Entrar" icon="pi pi-check" />
+            </div>
+        </>
+    )
+}
+export default Home; 
